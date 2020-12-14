c8cea75e2bcf07d20b7d42f868c0f16f99abb6a0
VavaIkelman <mr.ilkevich@gmai.com>
2020-12-14 16:54:36 +0300

    success for check

--- public/homePage.js
+++ public/homePage.js
@@ -2,7 +2,7 @@
 const logoutButton = new LogoutButton();
 logoutButton.action = () => {
     ApiConnector.logout(response => response.success && (location.reload()));
-};
+}
 
 ApiConnector.current(response => response.success && (ProfileWidget.showProfile(response.data)));
 
@@ -28,4 +28,63 @@ money.addMoneyCallback = data => {
             money.setMessage(response.success, response.error);
         };
     });
-};
+}
+
+money.conversionMoneyCallback = data => {
+    ApiConnector.convertMoney(data, response => {
+        if (response.success) {
+            ProfileWidget.showProfile(response.data);
+            money.setMessage(response.success, `Ковертация выполнена успешно`);
+        } else {
+            money.setMessage(response.success, response.error);
+        }
+    });
+}
+    
+money.sendMoneyCallback = data => {
+    ApiConnector.transferMoney(data, response => {
+        if (response.success) {
+            ProfileWidget.showProfile(response.data);
+            money.setMessage(response.success, "Перевод выполнен успешно");
+        } else {
+            money.setMessage(response.success, response.error);
+        };
+    });
+}
+
+const favorite = new FavoritesWidget();
+ApiConnector.getFavorites(response => {
+    if (response.success) {
+        favorite.clearTable();
+        favorite.fillTable(response.data);
+        money.updateUsersList(response.data);
+    } else {
+        favorite.setMessage(response.error);
+    }
+});
+
+favorite.addUserCallback = data => {
+    ApiConnector.addUserToFavorites(data, response => {
+        if (response.success) {
+            favorite.clearTable();
+            favorite.fillTable(response.data);
+            money.updateUsersList(response.data);
+            favorite.setMessage(response.success,`Пользователь успешно добавлен`);
+        } else {
+            favorite.setMessage(response.success, response.error);
+        }
+    });
+}
+
+favorite.removeUserCallback = id => {
+    ApiConnector.removeUserFromFavorites(id, response => {
+        if (response.success){
+            favorite.clearTable();
+            favorite.fillTable(response.data);
+            money.updateUsersList(response.data);
+            favorite.setMessage(response.success,`Пользователь успешно удален`);
+        } else {
+            favorite.setMessage(response.success, response.error);
+        }
+    });
+}
