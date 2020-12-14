363a781bceb434ef061ebf4554d9cc6369c45bbb
VavaIkelman <mr.ilkevich@gmai.com>
2020-12-14 15:40:19 +0300

    Login - success & homePage added MoneyManager

--- public/homePage.js
+++ public/homePage.js
@@ -1,2 +1,31 @@
-const userLogout = new LogoutButton;
-userLogout.action = ApiConnector.logout();
+'use strict'
+const logoutButton = new LogoutButton();
+logoutButton.action = () => {
+    ApiConnector.logout(response => response.success && (location.reload()));
+};
+
+ApiConnector.current(response => response.success && (ProfileWidget.showProfile(response.data)));
+
+const rate = new RatesBoard();
+function getExchange () { 
+ApiConnector.getStocks(response => {
+    if (response.success){
+        rate.clearTable(),
+        rate.fillTable(response.data)
+    };
+});
+}
+getExchange();
+setInterval(() => (getExchange, 600000));
+
+let money = new MoneyManager();
+money.addMoneyCallback = data => {
+    ApiConnector.addMoney(data, response => {
+        if (response.success) {
+           ProfileWidget.showProfile(response.data); 
+           money.setMessage(response.success, `Счет пополнен`);
+        } else {
+            money.setMessage(response.success, response.error);
+        };
+    });
+};
--- public/loginPage.js
+++ public/loginPage.js
@@ -1,4 +1,10 @@
-"use strict"
-const userForm = new UserForm();
-userForm.loginFormCallback = userForm.getData(userForm.loginForm);
-userForm.registerFormCallback = userForm.getData(userForm.registerForm);
+
+'use strict'
+let userForm = new UserForm();
+userForm.loginFormCallback = data => {
+	ApiConnector.login(data, response => response.success ? location.reload() : userForm.setLoginErrorMessage(response.error));
+};
+
+userForm.registerFormCallback = data => {
+    ApiConnector.register(data, response => response.success ? location.reload() : userForm.setRegisterErrorMessage(response.error));
+};
